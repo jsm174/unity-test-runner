ec6f20d97fffb07af453ee19eb876c4382eee5d6
jsm174 <jsm174@gmail.com>
2021-07-15 17:45:44 -0400

    parser: update annotation point to use first non-zero line number

--- src/model/results-parser.js
+++ src/model/results-parser.js
@@ -112,6 +112,23 @@ class ResultsParser {
   }
 
   static findAnnotationPoint(trace) {
+    const items = trace.match(/at .* in ((?<path>[^:]+):(?<line>\d+))/g);
+    if (Array.isArray(items)) {
+      const result = [];
+      items.forEach(item => {
+        const match = item.match(/at .* in ((?<path>[^:]+):(?<line>\d+))/);
+        const point = {
+          path: match ? match.groups.path : '',
+          line: match ? Number(match.groups.line) : 0,
+        };
+        if (point.line > 0) {
+          result.push(point);
+        }
+      });
+      if (result.length > 0) {
+        return result[0];
+      }
+    }
     const match = trace.match(/at .* in ((?<path>[^:]+):(?<line>\d+))/);
     return {
       path: match ? match.groups.path : '',
